1af89e7135d283bfdc161694202dd33ca2e3eea5
tinyshan <tinyshan@tencent.com>
2021-05-07 18:45:25 +0800

    一个UGUI的滑动列表效果实现

---
+++ Assets/Script/ListViewOffsetAnimation.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System;
+
+namespace UIWidgets
+{
+	/// <summary>
+	/// ListViewItem.
+	/// Item for ListViewBase.
+	/// </summary>
+	//[RequireComponent(typeof(Graphic))]
+	public class ListViewOffsetAnimation : UIBehaviour
+	{
+		[Title("最大缩放比例", "#FF4F63")]
+		public float mMaxScale = 1;
+		[Title("最小缩放比例", "#FF4F63")]
+		public float mMinScale = 0.8f;
+		[Title("缩放和描点距离比例计算系数", "#FF4F63")]
+		public float mScaleRate = 0.1f;
+
+		[Title("位移和缩放的节点", "#FF4F63")]
+		public RectTransform mOffsetTran;
+
+		[Title("相对锚点", "#FF4F63")]
+		public RectTransform mRefTran;
+
+		[Title("位移和锚点距离比例", "#FF4F63")]
+		public float mReferenceDelta = 0;
+
+		[Title("统一位移", "#FF4F63")]
+		public float mOffset = 0;
+
+		[Title("是否为纵向滑动列表", "#FF4F63")]
+		public bool mIsVertical = true;
+		private Vector2 mOffsetTranOrignPos;
+		private Vector2 mOffsetTranNewPos = Vector2.zero;
+		private Vector3 mScale = Vector3.one;
+		protected override void Awake()
+		{
+			this.mOffsetTranOrignPos = this.mOffsetTran.anchoredPosition;
+		}
+
+		void LateUpdate()
+		{
+			UpdateChildAnimationPostion();
+		}
+
+		private void UpdateChildAnimationPostion()
+		{
+			if (this.mOffsetTran == null)
+				return;
+
+			float usePos = this.mIsVertical ? this.transform.position.y : this.transform.position.x;
+			float targePos = this.mIsVertical ? this.mRefTran.position.y : this.mRefTran.position.x;
+			float absOffset = Mathf.Abs(usePos - targePos);
+			float offset = absOffset * this.mReferenceDelta + this.mOffset;
+			float scale = this.mMaxScale - absOffset * mScaleRate;
+			if (scale < this.mMinScale)
+				scale = this.mMinScale;
+			mScale.x = scale;
+			mScale.y = scale;
+			mScale.z = scale;
+
+			this.mOffsetTran.localScale = mScale;
+			if (this.mIsVertical)
+			{
+				mOffsetTranNewPos.x = this.mOffsetTranOrignPos.x + offset;
+				mOffsetTranNewPos.y = this.mOffsetTranOrignPos.y;
+			}
+			else
+			{
+				mOffsetTranNewPos.x = this.mOffsetTranOrignPos.x;
+				mOffsetTranNewPos.y = this.mOffsetTranOrignPos.y + offset;
+			}
+			this.mOffsetTran.anchoredPosition = mOffsetTranNewPos;
+		}
+	}
+}
---
+++ Assets/Script/ScrollUi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollUi : MonoBehaviour
+{
+    public Transform mRoot;
+    public GameObject mTemplete;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mTemplete.SetActive(false);
+        DateTime dtNow = DateTime.Now;
+        for (int i = 0; i < 50; ++i)
+        {
+            int rank = i + 1;
+            GameObject obj = GameObject.Instantiate(this.mTemplete);
+            obj.SetActive(true);
+            obj.transform.SetParent(mRoot);
+            obj.transform.localScale = Vector3.one;
+            obj.transform.Find("Content/Rank").GetComponent<Text>().text = "排名" + rank;
+            int score = UnityEngine.Random.Range(20000, 10000000);
+            obj.transform.Find("Content/Score").GetComponent<Text>().text = score.ToString();
+            int level = UnityEngine.Random.Range(1, 99);
+            obj.transform.Find("Content/Level").GetComponent<Text>().text = "等级" + level;
+            int randomAdd = UnityEngine.Random.Range(-100, 100);
+            DateTime dt = dtNow.AddDays(randomAdd).AddSeconds(i * randomAdd);
+            obj.transform.Find("Content/Date").GetComponent<Text>().text = dt.ToUniversalTime().ToString();
+        }
+    }
+}
